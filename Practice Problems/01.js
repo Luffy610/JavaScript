7b5c0a04bb59a4384518b3750c6cce43d2b5dfe5
Luffy610 <dhruvkotwani@outlook.com>
2024-11-05 23:02:04 +0530

    1st practice problem

---
+++ Practice Problems/01.js
@@ -0,0 +1,5 @@
+// Q1. Get a user to input a number using a prompt("Enter a number"). Check if the number is a multiple of 5 or not
+
+let number = prompt("Enter the number");
+
+number % 5 === 0 ? alert("Number is divisible by 5") : alert("Number is not divisible by 5");
